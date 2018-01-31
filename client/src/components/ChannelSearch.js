33560c87f89ef4fff8daff33ace12d5e9d09a5a9
joshua-murphy <josh.murphy11@gmail.com>
2018-01-30 20:22:27 -0700

    add channel and subscribe

--- client/src/components/ChannelSearch.js
+++ client/src/components/ChannelSearch.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { APIKey } from './APIKey';
+import { connect } from 'react-redux'
+import { addChannel } from '../actions/channels';
 import axios from 'axios';
-import { Card, Dimmer, Form, Grid, Image, Label, Loader } from 'semantic-ui-react';
+import { Button, Card, Dimmer, Form, Grid, Image, Label, Loader } from 'semantic-ui-react';
 
 class ChannelSearch extends React.Component {
 
-  state = { channel: {}, search: '', searchType: 'forUsername', loaded: true }
+  state = { channel: {}, description: '', search: '', searchType: 'forUsername', loaded: true }
 
   handleSearch = () => {
-    this.setState({ channel: {}, loaded: false })
+    this.setState({ channel: {}, description: '', loaded: false })
     const { searchType } = this.state
     axios.get(`https://www.googleapis.com/youtube/v3/channels?part=snippet&${searchType}=${this.state.search}&key=${APIKey}&fields=items(id,snippet(title,description,thumbnails/default))`)
       .then( res => {
@@ -16,13 +18,17 @@ class ChannelSearch extends React.Component {
       })
   }
 
+  addChannel = (e) => {
+    this.props.dispatch(addChannel(this.state.channel))
+  }
+
   adaptResponse = (data) => {
     if( data ) {
       const yt_channel_id = data.id
       const { title, description } = data.snippet
       const profile_image = data.snippet.thumbnails.default.url
-      const channel = { yt_channel_id, title, description, profile_image }
-      this.setState({ channel, loaded: true })
+      const channel = { yt_channel_id, title, profile_image }
+      this.setState({ channel, description, loaded: true })
     } else {
       this.setState({ error: true, loaded: true })
     }
@@ -33,14 +39,14 @@ class ChannelSearch extends React.Component {
   }
 
   render() {
-    const { channel, loaded } = this.state
+    const { channel, description, loaded } = this.state
     return (
       <Grid>
         <Grid.Row>
           <Grid.Column width={8}>
             <Form onSubmit={this.handleSearch}>
               <Label basic color='grey' 
-                content='Search for Channel ID by username ( youtube.com/user/<username> )' 
+                content='Search for channel by username ( youtube.com/user/<username> )' 
               />
               <Form.Group>
               <Form.Input
@@ -53,25 +59,30 @@ class ChannelSearch extends React.Component {
                 basic
                 icon='search'
                 onClick={this.handleSearch}
-                style={{marginBottom: 0}}
                 width={2}
               />
               </Form.Group>
             </Form>
           </Grid.Column>
           <Grid.Column width={8}>
-            { loaded ? 
-              <Card fluid>
-                <Card.Content>
-                  <Image floated='left' size='tiny' src={channel.profile_image} />
-                  <Card.Header style={{fontSize: 30}} textAlign='right'>
-                    { channel.title }
-                  </Card.Header>
-                  <Card.Description textAlign='left'>
-                    { channel.description }
-                  </Card.Description>
-                </Card.Content>
-              </Card>
+            { loaded ?
+              channel.title ? 
+                <Card fluid>
+                  <Card.Content>
+                    <Grid.Column width={6}>
+                      <Image rounded floated='left' size='tiny' src={channel.profile_image} />
+                    </Grid.Column>
+                    <Grid.Column width={12}>
+                      <Card.Header style={{fontSize: 24, marginTop: 5, marginBottom: 10}} content={ channel.title } />
+                      <Card.Description content={ description } />
+                    </Grid.Column>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <Button floated="right" primary content="Add to List" onClick={this.addChannel} />
+                  </Card.Content>
+                </Card>
+              :
+                null
             : 
               <Dimmer active inverted><Loader>Loading channel...</Loader></Dimmer> 
             }
@@ -83,4 +94,10 @@ class ChannelSearch extends React.Component {
 
 }
 
-export default ChannelSearch
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(ChannelSearch)
--- client/src/components/Subscription.js
+++ client/src/components/Subscription.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import axios from 'axios';
+import { APIKey } from './APIKey';
 import { deleteSub } from '../actions/subscriptions';
+import axios from 'axios';
 import { connect } from 'react-redux';
-import { Icon, Segment } from 'semantic-ui-react';
+import { Grid, Header, Icon, Image, Segment } from 'semantic-ui-react';
 
 class Subscriptions extends React.Component {
 
-  state = { subscription: {}, loading: true }
+  state = { subscription: {}, stats: {}, loading: true }
 
   componentDidMount() {
+    console.log(this.props.sub)
     axios.get(`api/channels/${this.props.sub.channel_id}`)
-      .then( res => this.setState({ subscription: res.data, loading: false }))
+      .then( res => this.setState({ subscription: res.data, loading: false }, () => axios.get(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${this.state.subscription.yt_channel_id}&key=${APIKey}&fields=items/statistics(viewCount,subscriberCount)`)
+        .then( res => this.setState({ stats: res.data.items[0].statistics }) )
+      ) )
       .catch( err => this.setState({ loading: false }))
   }
 
@@ -20,12 +24,29 @@ class Subscriptions extends React.Component {
       dispatch(deleteSub(sub))
   }
 
+  fixNumbers = (num = 0) => {
+    return parseInt(num, 10).toLocaleString()
+  }
+
   render() {
-    const { subscription, loading } = this.state
+    const { subscription, stats } = this.state
     return (
-      <Segment loading={ loading }>
-        {subscription.title}
-        <Icon link name="delete" style={{float: 'right'}} onClick={this.deleteSub} />
+      <Segment style={{width: "100%"}}>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={2}>
+              <Image circular src={subscription.profile_image} />
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <Header content={subscription.title} />
+              <p>{this.fixNumbers(stats.subscriberCount) || "0"}</p>
+              <p>{this.fixNumbers(stats.viewCount) || "0"}</p>
+            </Grid.Column>
+            <Grid.Column width={1}>
+              <Icon link name="delete" style={{float: 'right'}} onClick={this.deleteSub} />     
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Segment>
     )
   }
