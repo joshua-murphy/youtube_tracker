af202786e9c2e84f9aca3a01f8b58a054e6209e6
joshua-murphy <josh.murphy11@gmail.com>
2018-01-30 22:17:20 -0700

    site coloring

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,7 +13,7 @@ import { Switch, Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div style={{height: "100vh", backgroundColor: "#dbdbdb"}}>
         <NavBar />
         <Flash />
         <FetchUser>
