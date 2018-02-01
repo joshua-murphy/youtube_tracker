82dd51e2ebd534f08bb9639c0260f7179cadb110
joshua-murphy <josh.murphy11@gmail.com>
2018-01-31 20:11:06 -0700

    prevent duplicate channels

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,5 +1,6 @@
 class Channel < ApplicationRecord
-  validates_presence_of [:title, :yt_channel_id]
+  validates_presence_of [:title, :yt_channel_id, :user_id]
+  validates_uniqueness_of :yt_channel_id
 
   belongs_to :user
 end
